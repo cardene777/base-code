34fb74a61ca6a2687fa231416f669238095456ed
cardene <chotips08@gmail.com>
2023-07-31 01:51:54 +0900

    🚀 add ERC3525

--- contract/docs/test.md
+++ contract/docs/test.md
@@ -4,6 +4,12 @@
 npx hardhat test test/ERC2981/ERC2981.test.ts
 ```
 
+## ERC3525
+
+```sh
+npx hardhat test test/ERC3525/ERC3525.test.ts
+```
+
 ## ERC6147
 
 ```sh
---
+++ contract/test/SampleBase.test.ts
@@ -0,0 +1,38 @@
+import { ethers } from 'hardhat';
+import { expect } from 'chai';
+import { BigNumber } from '@ethersproject/bignumber';
+import '@nomicfoundation/hardhat-chai-matchers';
+import { Sample } from "../types";
+
+let owner: any;
+let addr1;
+let addr2;
+let initialOwnerBalance: BigNumber;
+
+describe('testing for ERC20', async () => {
+  let contract: Sample;
+
+  beforeEach(async () => {
+    const signers = await ethers.getSigners();
+    owner = signers[0];
+    addr1 = signers[1];
+    addr2 = signers[2];
+
+    const tokenName = 'QYS Coin';
+    const tokenSymbol = 'QYS';
+
+    // Contractのデプロイ
+    const Sample = await ethers.getContractFactory('Sample');
+    contract = (await Sample.deploy(tokenName, tokenSymbol, owner.address)) as Sample;
+
+    initialOwnerBalance = await contract.balanceOf(owner.address);
+  });
+
+  // ERC20Tokenの初期 Mint数とERC20Tokenの発行総量が等しいか
+  describe('Deployment', function () {
+    it('Should assign the total supply of tokens to the owner', async function () {
+      const ownerBalance = await contract.balanceOf(owner.address);
+      expect(await contract.totalSupply()).to.equal(ownerBalance);
+    });
+  });
+});
